99fd6f59738b19690b074663662e10d33d356d44
libing <libing@zto.cn>
2019-05-29 14:27:32 +0800

    引入 element-ui样式

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import Element from 'element-ui'; // 引入element ui
+import Element from 'element-ui' // 引入element ui
+import 'element-ui/lib/theme-chalk/index.css' // 引入 element ui 样式
 
 Vue.config.productionTip = false
 Vue.use(Element, {
