0c534e060eb1fb330437816b2ec42c92135d1b3b
libing <libing@zto.cn>
2019-05-29 14:21:56 +0800

    引入 element-ui

--- src/main.js
+++ src/main.js
@@ -4,7 +4,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Element from 'element-ui'; // 引入element ui
+
 Vue.config.productionTip = false
+Vue.use(Element, {
+  size: 'mini' // set element-ui default size
+})
 
 /* eslint-disable no-new */
 new Vue({
