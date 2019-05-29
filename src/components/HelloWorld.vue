191639ea8946afce4b2f71e4d968064451b5abce
libing <libing@zto.cn>
2019-05-29 14:35:30 +0800

    基础表格展示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="hello">
     {{msg}}
+    <div>
+      <el-button
+        type="primary"
+        size="mini"
+        style="margin:30px auto;"
+      >导出</el-button>
+      <el-table
+        :data="tableData"
+        stripe
+        style="width: 600px;margin:30px auto;"
+      >
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180"
+        >
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+        >
+        </el-table-column>
+      </el-table>
+    </div>
   </div>
 </template>
 
@@ -9,7 +39,28 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Excel Export'
+      msg: 'Welcome to Your Excel Export',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
+        },
+        {
+          date: '2016-05-04',
+          name: '李小虎',
+          address: '上海市普陀区金沙江路 1517 弄'
+        },
+        {
+          date: '2016-05-01',
+          name: '孙小虎',
+          address: '上海市普陀区金沙江路 1519 弄'
+        },
+        {
+          date: '2016-05-03',
+          name: '赵小虎',
+          address: '上海市普陀区金沙江路 1516 弄'
+        }]
     }
   }
 }
@@ -17,7 +68,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1,
+h2 {
   font-weight: normal;
 }
 ul {
