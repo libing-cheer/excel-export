c16f91a0a502f3859d5122f8d36b7b2d73ed76dc
libing-cheer <libing_cheer@163.com>
2019-06-08 18:15:40 +0800

    多表头展示信息

--- src/components/excelExport.vue
+++ src/components/excelExport.vue
@@ -16,19 +16,39 @@
         <el-table-column
           prop="date"
           label="日期"
-          width="180"
+          width="100"
         >
         </el-table-column>
         <el-table-column
           prop="name"
           label="姓名"
-          width="180"
+          width="100"
         >
         </el-table-column>
         <el-table-column
           prop="address"
           label="地址"
+          width="190"
         >
+        </el-table-column>
+
+        <el-table-column
+          label="个人信息"
+          width="180"
+          align="center"
+        >
+          <el-table-column
+            prop="age"
+            label="年龄"
+            align="center"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="height"
+            label="身高"
+            align="center"
+          >
+          </el-table-column>
         </el-table-column>
       </el-table>
     </div>
@@ -45,37 +65,51 @@ export default {
         {
           date: '2016-05-02',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1510 弄'
+          address: '上海市普陀区金沙江路 1510 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-04',
           name: '李小虎',
-          address: '上海市普陀区金沙江路 1511 弄'
+          address: '上海市普陀区金沙江路 1511 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-01',
           name: '孙小虎',
-          address: '上海市普陀区金沙江路 1512 弄'
+          address: '上海市普陀区金沙江路 1512 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-02',
           name: '朱小虎',
-          address: '上海市普陀区金沙江路 1513 弄'
+          address: '上海市普陀区金沙江路 1513 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-04',
           name: '钱小虎',
-          address: '上海市普陀区金沙江路 1514 弄'
+          address: '上海市普陀区金沙江路 1514 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-01',
           name: '杜小虎',
-          address: '上海市普陀区金沙江路 1515 弄'
+          address: '上海市普陀区金沙江路 1515 弄',
+          age: 18,
+          height: '180cm'
         },
         {
           date: '2016-05-03',
           name: '赵小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
+          address: '上海市普陀区金沙江路 1516 弄',
+          age: 18,
+          height: '180cm'
         }],
       tableTitleData: [
         {
@@ -89,6 +123,14 @@ export default {
         {
           label: '地址',
           prop: 'address'
+        },
+        {
+          label: '年龄',
+          prop: 'age'
+        },
+        {
+          label: '身高',
+          prop: 'height'
         }
       ]
     }
